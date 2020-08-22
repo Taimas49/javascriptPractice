38649268062dea34e9ef7fbd6fdaaf5882a0eb3d
Taimas <urazgalit@gmail.com>
2020-08-22 18:08:33 +0600

    Added tabs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 window.addEventListener('DOMContentLoaded', () => {
     const tabsParent = document.querySelector('.tabheader__items');
     const tabs = document.querySelectorAll('.tabheader__item');
-    const tabsContetn = document.querySelectorAll('.tabcontent');
+    const tabsContent = document.querySelectorAll('.tabcontent');
+    console.log(tabsContent);
+
+    function hideTabContent () {
+        tabsContent.forEach(item => {
+            item.style.display = "none";
+        });
+        tabs.forEach( item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+
+    function showTabContent (i = 0) { // Если функ. вызывается без параметра то здесь можно задать default знач.
+        tabsContent[i].style.display = "block";
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+
 });
