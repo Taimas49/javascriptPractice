c987b0cee1184423ab27a3d249f8458a2ac66204
Taimas <urazgalit@gmail.com>
2020-08-22 17:35:49 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabsParent = document.querySelector('.tabheader__items');
+    const tabs = document.querySelectorAll('.tabheader__item');
+    const tabsContetn = document.querySelectorAll('.tabcontent');
+});
