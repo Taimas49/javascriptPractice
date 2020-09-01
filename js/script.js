be4a5855404b177de94ae69bdc26589a75d2aaf0
Taimas <urazgalit@gmail.com>
2020-09-01 14:26:07 +0600

    Modal window and timer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('DOMContentLoaded', () => {
     const tabsParent = document.querySelector('.tabheader__items');
     const tabs = document.querySelectorAll('.tabheader__item');
@@ -34,4 +35,98 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
+// Таймер 
+const deadline = '2020-08-30';
+
+function getTimeRemaining (endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60 ) % 24));
+    const minutes = Math.floor((t / 1000 / 60 ) % 60);
+    const sec = Math.floor((t / 1000) % 60);
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': sec
+    };
+}
+function getZero (num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+    function setClock (selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector('#days');
+        const hours = timer.querySelector('#hours');
+        const minutes = timer.querySelector('#minutes');
+        const seconds = timer.querySelector('#seconds');
+        const timeInterval = setInterval(updateClock, 1000);
+        updateClock();
+        function updateClock () {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+            if (t.total <= 0 ){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', deadline);
+
+
+    //Модальное окно 
+    const modalWindow = document.querySelector('.modal');
+    const modalClose = modalWindow.querySelector('[data-close]');
+    const modalTrigger = document.querySelectorAll('[data-modal]');
+    console.log(modalTrigger);
+
+    function closeModal() {
+        modalWindow.classList.add('hide');
+        modalWindow.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+    function openModal () {
+        modalWindow.classList.add('show');
+        modalWindow.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+    modalTrigger.forEach(item => {
+        item.addEventListener('click', openModal);
+    });
+
+    modalClose.addEventListener ('click', closeModal);
+
+    modalWindow.addEventListener('click', (event) => {
+        if (event.target === modalWindow) {
+            closeModal();
+        }
+    });
+
+
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modalWindow.classList.contains('show')) {
+            closeModal();
+        }
+    });
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll () {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
 });
