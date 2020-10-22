05a195ff5610761e1834f20bb06c4a0e793cb8d2
Taimas <urazgalit@gmail.com>
2020-10-22 14:59:23 +0600

    bug fix

---
+++ js/Conspects/eventLoop.js
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log(1);
+
+setTimeout(() => {}, 4000);
---
+++ js/Conspects/generator.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function* generator() {
+    yield 'S';
+    yield 'c';
+    yield 'r';
+    yield 'i';
+    yield 'p';
+    yield 't';
+}
+const str = generator();
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
